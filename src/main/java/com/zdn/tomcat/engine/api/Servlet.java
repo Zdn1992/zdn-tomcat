336fde139139a90c1d7e9dc90b406b3dd134d898
zhudanning <zhudanning@sm.vvip-u.com>
2019-03-28 14:35:11 +0800

    在dev分支上提交代码

--- src/main/java/com/zdn/tomcat/engine/api/Servlet.java
+++ src/main/java/com/zdn/tomcat/engine/api/Servlet.java
@@ -3,6 +3,12 @@ package com.zdn.tomcat.engine.api;
 import com.zdn.tomcat.connector.ServletRequest;
 import com.zdn.tomcat.connector.ServletResponse;
 
+/**
+ * @Description: javax servlet规范
+ * @Author: zdn
+ * @CreateDate: 2019/3/28
+ * @Version: 1.0
+ */
 public interface Servlet {
 
     void init();
